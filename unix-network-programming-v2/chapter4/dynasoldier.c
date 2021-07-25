5104ff74c3dadd4478c4d2c1b717bcf49f49b633
wayne.wang <yingxiang907@gmail.com>
2021-07-25 14:40:46 +0800

    fix bug

--- unix-network-programming-v2/chapter4/dynasoldier.c
+++ unix-network-programming-v2/chapter4/dynasoldier.c
@@ -5,10 +5,8 @@ int main(int argc, char **argv) {
   char msg[1024];
   int n;
   while((n = read(STDIN_FILENO, msg, sizeof(msg))) > 0) {
-      if (msg[n - 1] == '\n') {
-        msg[n - 1] = '\0';
-      }
-      printf("%s\n", msg);
+      msg[n] = '\0';
+      printf("%s", msg);
   }
   printf("dynasoldier\n");
 }
