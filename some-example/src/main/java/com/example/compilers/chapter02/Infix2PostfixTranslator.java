b2a240f12ca5439142cd573878653fe101f601d4
wayne.wang <wayne.wang@beibei.com>
2022-09-21 19:37:43 +0800

    1

---
+++ some-example/src/main/java/com/example/compilers/chapter02/Infix2PostfixTranslator.java
@@ -0,0 +1,17 @@
+package com.example.compilers.chapter02;
+
+public class Infix2PostfixTranslator {
+
+    public static void main(String[] args) {
+        Parser parser = new Parser();
+
+    }
+
+    static class Parser {
+        static int lookahead;
+
+        Parser() {
+
+        }
+    }
+}
--- some-example/src/main/java/com/example/compilers/chapter02/PrefixParser.java
+++ some-example/src/main/java/com/example/compilers/chapter02/PrefixParser.java
@@ -7,71 +7,53 @@ import java.util.Objects;
  *    | - S S
  *    | 0 | 1 | 2 | 3 | 4
  */
-public class Translation {
+public class PrefixParser {
 
     private char lookahead;
     private int lookaheadIndex = 0;
     private final String input = "- 2 + 1 + - 2 4 4";
 
     public static void main(String[] args) {
-        new Translation().parse();
+        new PrefixParser().parse();
     }
 
     public void parse() {
         lookahead = input.charAt(lookaheadIndex);
-        Node root = s();
-        System.out.printf("infix expr: %s\neval value: %s\n", root.t, root.v);
+        s();
     }
 
-    public Node s() {
-        Node node = new Node();
-        Node s1, s2;
+    public void s() {
         switch (lookahead) {
             case '+':
                 match('+');
-                s1 = s();
-                s2 = s();
-                node.t = String.format("(%s + %s)", s1.t, s2.t);
-                node.v = s1.v + s2.v;
+                s();
+                s();
                 break;
             case '-':
                 match('-');
-                s1 = s();
-                s2 = s();
-                node.t = String.format("(%s - %s)", s1.t, s2.t);
-                node.v = s1.v - s2.v;
+                s();
+                s();
                 break;
             case '0':
                 match('0');
-                node.t = "0";
-                node.v = 0;
                 break;
             case '1':
                 match('1');
-                node.t = "1";
-                node.v = 1;
                 break;
             case '2':
                 match('2');
-                node.t = "2";
-                node.v = 2;
                 break;
             case '3':
                 match('3');
-                node.t = "3";
-                node.v = 3;
                 break;
             case '4':
                 match('4');
-                node.t = "4";
-                node.v = 4;
                 break;
             case '\0':
                 break;
             default:
                 throw new RuntimeException("Syntax error");
         }
-        return node;
     }
 
     public void match(char terminal) {
