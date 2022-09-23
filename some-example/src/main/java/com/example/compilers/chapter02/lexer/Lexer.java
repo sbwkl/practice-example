b049800a1c19135ef9fadfc69ab54e7b942d4545
wayne.wang <wayne.wang@beibei.com>
2022-09-23 19:13:35 +0800

    1

--- some-example/src/main/java/com/example/compilers/chapter02/lexer/Lexer.java
+++ some-example/src/main/java/com/example/compilers/chapter02/lexer/Lexer.java
@@ -1,12 +1,16 @@
 package com.example.compilers.chapter02.lexer;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.List;
 import java.util.Map;
 
 public class Lexer {
     public int line = 1;
     private char peek = ' ';
+    List<Character> buffer = new ArrayList<>();
+    private int bufferIndex = 0;
     private Map<String, Token> words = new Hashtable<>();
     void reserve(Word w) {
         words.put(w.lexeme, w);
@@ -16,16 +20,19 @@ public class Lexer {
         reserve(new Word(Tag.FALSE, "false"));
     }
     public Token scan() throws IOException {
-        for (; ; peek = (char) System.in.read()) {
+        for (; ; peek = nextCharacter()) {
             if (peek == ' ' || peek == '\t') continue;
             else if (peek == '\n') line += 1;
+            else if (peek == '/') {
+                buffer.add(peek);
+            }
             else break;
         }
         if (Character.isDigit(peek)) {
             int v = 0;
             do {
                 v = 10 * v + Character.digit(peek, 10);
-                peek = (char) System.in.read();
+                peek = nextCharacter();
             } while (Character.isDigit(peek));
             return new Num(v);
         }
@@ -33,7 +40,7 @@ public class Lexer {
             StringBuffer b = new StringBuffer();
             do {
                 b.append(peek);
-                peek = (char) System.in.read();
+                peek = nextCharacter();
             } while (Character.isLetterOrDigit(peek));
             String s = b.toString();
             Word w = (Word) words.get(s);
@@ -46,4 +53,12 @@ public class Lexer {
         peek = ' ';
         return t;
     }
+
+    private char nextCharacter() throws IOException {
+        if (buffer.isEmpty()) {
+            buffer.add((char) System.in.read());
+        }
+        return buffer.get(0);
+    }
+
 }
