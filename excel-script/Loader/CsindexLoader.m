60ba0f741592b4a481f8585ad5aa436ba96a0d2c
wayne.wang <1007677085@qq.com>
2026-02-19 08:45:55 +0800

    统一列名

--- excel-script/Loader/CsindexLoader.m
+++ excel-script/Loader/CsindexLoader.m
@@ -2,13 +2,15 @@
 let
     Today = DateTime.ToText(DateTime.LocalNow(), "yyyyMMdd"),
     TargetUrl = "https://www.csindex.com.cn/csindex-home/perf/index-perf?indexCode=" & code & "&startDate=20150101&endDate=" & Today,
-    Source = Json.Document(Web.Contents(TargetUrl)),
+    Source = Json.Document(Web.Contents(TargetUrl, [Headers=[
+        #"User-Agent"="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36"
+    ]])),
     JsonData = Source[data],
     Data = Table.FromRecords(JsonData),
     Selected = Data[[tradeDate], [close]],
-    Renamed = Table.RenameColumns(Selected, {{"tradeDate", "date"}, {"close", "point"}}),
+    Renamed = Table.RenameColumns(Selected, {{"tradeDate", "date"}, {"close", "value"}}),
     TypeDate = Table.TransformColumnTypes(Renamed, {{"date", type date}}),
-    Typed = Table.TransformColumnTypes(TypeDate, {{"point", type number}}),
+    Typed = Table.TransformColumnTypes(TypeDate, {{"value", type number}}),
     Final = Table.Sort(Typed,{{"date", Order.Descending}})
 in
     Final
